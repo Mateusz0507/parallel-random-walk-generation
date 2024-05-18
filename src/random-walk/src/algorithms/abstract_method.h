19295a06da58ea67e369e89f62d609eb96c7008a
xNetru <kamilbbank@gmail.com>
2024-05-18 16:27:01 +0200

    made run public again

--- src/random-walk/src/algorithms/abstract_method.h
+++ src/random-walk/src/algorithms/abstract_method.h
@@ -6,10 +6,10 @@ namespace algorithms
 	class abstract_method
 	{
 	public:
-		virtual bool run(vector3** result, int N) = 0;
+		virtual bool run(vector3** result, void*) = 0;
 	protected:
 		void cuda_release(void** dev_ptr);
 		void cuda_allocate(void** dev_ptr, int size, bool* allocation_failure);
-		virtual bool run(vector3** result, void*) = 0;
+		
 	};
 }
