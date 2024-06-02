a775cab53ed351cd97c3f2c50e1f29a070f04e3d
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-06-02 18:19:55 +0200

    chore: remove default arguments values from function generating starting positions

--- src/random-walk/src/algorithms/model/directional_randomization.cuh
+++ src/random-walk/src/algorithms/model/directional_randomization.cuh
@@ -25,7 +25,7 @@ namespace algorithms
 	namespace directional_randomization
 	{
 		bool generate_starting_positions(vector3* dev_unit_vectors_argument, vector3* dev_points_argument,
-			const int N, const int directional_parametr = 0, const int number_of_segments = 0);
+			const int N, const int directional_parametr, const int number_of_segments);
 		__global__ void kernel_generate_segments_directions(matrix* dev_segments_directions_matrices,
 			curandState* dev_states, int number_of_segments, uint64_t seed);
 		__global__ void kernel_setup(curandState* dev_states, int N, uint64_t seed, uint64_t offset);
