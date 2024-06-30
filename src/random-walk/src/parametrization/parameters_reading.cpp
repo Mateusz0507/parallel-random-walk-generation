b9d27f04932762d991e2c2821b7d5deceaf930b7
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-06-30 22:37:05 +0200

    fix: add conditions for parametrization

--- src/random-walk/src/parametrization/parameters_reading.cpp
+++ src/random-walk/src/parametrization/parameters_reading.cpp
@@ -5,12 +5,12 @@ void program_parametrization::print_usage(const char* name)
 {
     std::cerr << "Usage " << name << ":" << std::endl
     << "[-m/--method]=[naive/normalization/genetic/genetic2] (default:naive)" << std::endl
-    << "[-N/--N]=[int >=3] (default:100)" << std::endl
-    << "[-d/--directional-level]=[int] (default:0)" << std::endl
-    << "[-s/--segments-number]=[int] (default:1)" << std::endl
+    << "[-N/--N]=[int >= 3] (default:100)" << std::endl
+    << "[-d/--directional-level]=[int >= 0] (default:0)" << std::endl
+    << "[-s/--segments-number]=[int >= 1] (default:1)" << std::endl
     << "Parameters for genetic/genetic2 method only:" << std::endl
-    << "   --mutation-ratio=[float] (default:0.05)" << std::endl
-    << "   --generation-size=[int] (default:10)" << std::endl;
+    << "   --mutation-ratio=[float >= 0] (default:0.05)" << std::endl
+    << "   --generation-size=[int >= 8] (default:10)" << std::endl;
 }
 
 bool program_parametrization::read(int argc, char** argv, parameters& p)
@@ -39,23 +39,43 @@ bool program_parametrization::read(int argc, char** argv, parameters& p)
             p.N = atoi(value);
             if (p.N < 3)
             {
-                std::cerr << "Invalid N parameter value: N >= 3" << std::endl;
+                std::cerr << "Invalid parameter value: N >= 3" << std::endl;
                 return false;
             }
         }
         else if (std::string(parameter) == "-d" || std::string(parameter) == "--directional-level") {
             p.directional_level = atoi(value);
+            if (p.directional_level < 0)
+            {
+                std::cerr << "Invalid parameter value: directional-level >= 0" << std::endl;
+                return false;
+            }
         }
         else if (std::string(parameter) == "-s" || std::string(parameter) == "--segments-number") {
             p.segments_number = atoi(value);
+            if (p.segments_number < 1)
+            {
+                std::cerr << "Invalid parameter value: segments-number >= 1" << std::endl;
+                return false;
+            }
         }
         else if (std::string(p.method) == "genetic" || std::string(p.method) == "genetic2")
         {
             if (std::string(parameter) == "--mutation-ratio") {
                 p.mutation_ratio = atof(value);
+                if (p.mutation_ratio < 0)
+                {
+                    std::cerr << "Invalid parameter value: mutation-ratio >= 0" << std::endl;
+                    return false;
+                }
             }
             else if (std::string(parameter) == "--generation-size") {
                 p.generation_size = atoi(value);
+                if (p.generation_size < 8)
+                {
+                    std::cerr << "Invalid parameter value: generation-size >= 8" << std::endl;
+                    return false;
+                }
             }
             else
             {
